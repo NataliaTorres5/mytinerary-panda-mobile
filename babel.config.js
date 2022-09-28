a7451763e5c85ffd9ce97f002a55dac3cc18ed8e
Natalia Torres <nataliatorres88@gmail.com>
2022-09-28 14:09:00 -0500

    added plugin

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo'], //configuracion de js  a otro js 
+    plugins: ['react-native-reanimated/plugin']
+
   };
 };
