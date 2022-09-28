2c913725a624d9b02e55edee922f3f7d938845e6
Natalia Torres <nataliatorres88@gmail.com>
2022-09-28 18:38:19 -0500

    fixed error

--- App.js
+++ App.js
@@ -1,24 +1,34 @@
 import { StatusBar } from 'expo-status-bar'; //maneja barra de estados 
 import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
-import Navigation from './src/components/Navigation';
+import { NavigationContainer } from '@react-navigation/native';
 import Home from './src/screen/Home';
 import Cities from './src/screen/Cities';
 import { Provider } from 'react-redux';
 import  Store  from './src/features/store';
+import Drawer from './src/navigation/Drawer';
 
 
 export default function App() {
 
   return (
 <Provider store={Store} >
-    <View style={styles.container}>
-      <Navigation style=""/>
+
+    <NavigationContainer>
+
+    <Drawer/>
+      
+    {/* <View style={styles.container}>
+      
       <Text>PANDA XIAO MEI!</Text>
       <Cities/>
       <StatusBar style="dark" />
-    </View>
+    </View> */}
+    </NavigationContainer>
+    
     </Provider>
+
+
   );
 }
 
