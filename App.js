cc710443634ea6d3049d5f11a982354f8a5572ae
LauraMartinez20 <danielamartinez.1012@gmail.com>
2022-09-28 14:50:23 -0500

    added libreries, and create welcome component

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Welcome from './src/Components/Welcome'
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Cohort 31</Text>
       <StatusBar style="auto" />
+      <Welcome/>
     </View>
+    
+    
   );
 }
 
---
+++ src/Components/Welcome.js
@@ -0,0 +1,43 @@
+import { StatusBar } from 'expo-status-bar';
+import { ImageBackground, Image, StyleSheet, Text, View } from 'react-native';
+
+  const image = { uri: "https://i.im.ge/2022/09/28/1EujTY.photo-mobile.jpg" };
+  const logo = {uri: "https://i.im.ge/2022/09/28/1EFqw1.WelcomeIntro.png"};
+
+  const App =() => (
+    <View style={styles.container}>
+      <ImageBackground source={image}
+      resizeMode="cover" style={styles.image}>
+      <Image style={styles.logo} source={logo}/>
+      <Text style={styles.text}>Cohort 31</Text>
+      </ImageBackground> 
+    </View>
+    
+  );
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  logo: {
+    width: 350,
+    height: 350,
+    justifyContent: "center",
+    paddingLeft: 100,
+  },
+  text: {
+    color: "blue",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0"
+  }
+});
+
+export default Welcome;
