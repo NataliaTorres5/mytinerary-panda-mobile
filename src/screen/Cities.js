d9c4808afe50637f25c981481bbc342e855b2b93
Natalia Torres <nataliatorres88@gmail.com>
2022-09-28 18:40:17 -0500

    stablished connection with the id

--- src/screen/Cities.js
+++ src/screen/Cities.js
@@ -1,11 +1,19 @@
 import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import CityCards from '../components/CityCards'
+import { useNavigation } from '@react-navigation/native'
 
-export default function Cities() {
+export default function Cities(props) {
+
+    const navigation = useNavigation()
+
+
+
+    console.log(props)
     return (
         <ScrollView>
-            <CityCards />
+            <CityCards navigation={navigation} />
         </ScrollView>
     )
 }
+
