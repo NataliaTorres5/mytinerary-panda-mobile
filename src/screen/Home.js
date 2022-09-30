3ecfb8373a8201fe3be6fde62fa70b1d6415638d
Natalia Torres <nataliatorres88@gmail.com>
2022-09-29 23:04:33 -0500

    updated home

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -12,19 +12,3 @@ export default function Home() {
   )
 }
 
-const styles = StyleSheet.create({
-    
-    container: {
-      flex: 1,
-      backgroundColor: 'white',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    backgroundVideo: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-      },
-    });
