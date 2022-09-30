32db3611bcc325eec516f37906f50e1cb6913ad5
Natalia Torres <nataliatorres88@gmail.com>
2022-09-29 21:19:38 -0500

    created SignUp css missing

---
+++ src/screen/SignUp.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import SignUpContent from '../components/user/SignUp'
+
+export default function SignUp() {
+  return (
+    <View>
+      <SignUpContent/>
+    </View>
+  )
+}
