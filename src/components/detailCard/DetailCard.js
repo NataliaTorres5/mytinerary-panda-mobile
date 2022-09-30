3070a3f5b2d386d36d0193a95a0243505bfe143b
Natalia Torres <nataliatorres88@gmail.com>
2022-09-30 10:12:21 -0500

    added style

--- src/components/detailCard/DetailCard.js
+++ src/components/detailCard/DetailCard.js
@@ -26,24 +26,56 @@ export default function DetailCard(props) {
   return (
     <View style={{
       width: width,
-      backgroundColor: 'lightblue'
+      backgroundColor: '#636fa4',
+      padding: '2%',
+
     }} >
 
 
-      <View>
+      <View
+      style= {{
+        alignItems:'center',
+        backgroundColor: 'lavender',
+        borderRadius: 15,
+        padding: '2%',
+        
+      }}>
 
-        <Text>{City?.city}</Text>
-        <Text>{City?.country}</Text>
+        <Text
+        style={{
+          textAlign: 'center',
+          fontSize: 30
+        }} >{City?.city}</Text>
+        <Text
+          style={{
+            fontSize: 20,
+            fontStyle: "italic",
+            padding: '2%',
+          }} >{City?.country}</Text>
         <Image
           style={{
-            width: 50,
-            height: 50
+            width: 350,
+            height: 200,
+            borderRadius: 15,
+            padding: '2%',
           }}
           source={{ uri: City?.photo }} />
 
-        <Text>{City?.details}</Text>
-        <Text>Population : {City?.population}</Text>
-        <Text>{foundationYear}</Text>
+        <Text
+        style={{
+          padding: '2%',
+          textAlign:'justify',
+        }}>{City?.details}</Text>
+        <Text
+         style={{
+          padding: '2%',
+          textAlign:'justify',
+        }}>Population : {City?.population}</Text>
+        <Text
+         style={{
+          padding: '2%',
+          textAlign:'justify',
+        }}>Foundation time: {foundationYear}</Text>
 
       </View>
     </View>
