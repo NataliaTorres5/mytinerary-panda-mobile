f00bfbe844524dd679570a93a668ca830047b98a
LauraMartinez20 <danielamartinez.1012@gmail.com>
2022-09-29 23:10:54 -0500

    import carousel

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import CarouselHome from '../components/CarouselHome'
 import LandingHome  from '../components/LandingHome';
+import UnderConstruction from '../screen/UnderConstruction'
+
 
 export default function Home() {
 
   return (
    < View>
+    <CarouselHome/>
     <LandingHome/>
    </View>
   
   )
 }
+//<UnderConstruction/>
 
 const styles = StyleSheet.create({
     
